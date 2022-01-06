5bc917df1a2b8d80c6bd72e7203e3ec682791ccc
Jort Kuhlmann <jortkuhlmann1@gmail.com>
2022-01-06 19:15:58 +0100

    remove unused packages

---
+++ README.md
@@ -0,0 +1,1 @@
+docker run --name hello-world -it --rm -p 3000:3000 public.ecr.aws/u8i7q3h6/hello-world-docker-aws-ecr:3e099a90fe8ba8dd055a4a6e52c11546936f8fe0
--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/hello-world", async (req, res) => {
+app.get("/", async (req, res) => {
   res.send("Hello world!");
 });
 
