c26650f6717dd07c68a0c56d319358abb22d6a1a
Jort Kuhlmann <jortkuhlmann1@gmail.com>
2022-01-07 13:18:17 +0100

    add linter and jest (#5)
    * add linter and jest

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,27 @@
-import http from "http";
-import { createTerminus } from "@godaddy/terminus";
-import { logger } from "@mayntri/hello-world-architecture";
-import app from "./app";
+import http from 'http';
+import { createTerminus } from '@godaddy/terminus';
+import { logger } from '@mayntri/hello-world-architecture';
+import app from './app';
+
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
 createTerminus(server, {
-  signal: "SIGINT",
+  signal: 'SIGINT',
   healthChecks: {
-    "/healthcheck": () => Promise.resolve("UP"),
+    '/healthcheck': () => Promise.resolve('UP'),
   },
   onSignal: () => {
-    logger.log("server is starting cleanup");
+    logger.log('server is starting cleanup');
     return Promise.resolve();
   },
   onShutdown: () => {
-    logger.log("cleanup finished, server is shutting down");
+    logger.log('cleanup finished, server is shutting down');
     return Promise.resolve();
   },
 });
 
 server.listen(port, () => {
-  logger.log("Server started on port: " + port);
+  logger.log(`Server started on port: ${port}`);
 });
