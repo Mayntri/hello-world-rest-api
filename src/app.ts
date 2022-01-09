d5f0600624550a596f14ceec8677db02c756e53f
Jort Kuhlmann <jortkuhlmann1@gmail.com>
2022-01-09 20:52:47 +0100

    add k8s

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
+import { logger } from '@mayntri/hello-world-architecture';
 import express from 'express';
 
 const app = express();
 
 app.get('/', async (req, res) => {
+  logger.log('Hello moon and mars!');
   res.send('Hello moon and mars!');
 });
 
 app.get('/test', async (req, res) => {
+  logger.log('test');
   res.send('test');
 });
 
